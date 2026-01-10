c611773bc4c5f969ede980f9175e0c6eb955dcdb
shuvo81 <shuvomohajon81@gmail.com>
2026-01-10 23:54:00 +0600

    Activity bar fixed

--- script/activity-card_3.js
+++ script/activity-card_3.js
@@ -6,12 +6,22 @@ let currentTime = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-
 const assigntask = document.getElementById('assigned-task-count').innerText;   
 const assignTask = parseInt(assigntask);
 
+// if (assignTask <= 6) {
+//     const taskTitle = document.getElementById('taskTitle3').innerText;
+//     const container = document.getElementById('activity-container')
+//     const p = document.createElement("p");
+//     p.innerText = `You have Complete The Task ${taskTitle} at ${currentTime}`;
+//     container.appendChild(p).style.padding  = "10px";
+// }
+
 if (assignTask <= 6) {
     const taskTitle = document.getElementById('taskTitle3').innerText;
-    const container = document.getElementById('activity-container')
-    const p = document.createElement("p");
-    p.innerText = `You have Complete The Task ${taskTitle} at ${currentTime}`;
-    container.appendChild(p).style.padding  = "10px";
+    const container = document.getElementById('activity-container');
+
+    const item = document.createElement("div");
+    item.className = "activity-item";
+    item.innerText = `You have Complete The Task ${taskTitle} at ${currentTime}`;
+    container.appendChild(item);
 }
 },
 { once: true }
