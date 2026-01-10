1a62f6f50aaeb64be2ce9985fa86d64155bb43af
shuvo81 <shuvomohajon81@gmail.com>
2026-01-11 00:20:13 +0600

    Complete button disable after one click fix

--- script/card_6.js
+++ script/card_6.js
@@ -1,24 +1,16 @@
-document.getElementById('glassdoar-btn').addEventListener('click', function(){
-   
-    alert("Board updated successfully");
-    // const task = document.getElementById('navbar-task-count').innerText;    
-    // const assigntask = document.getElementById('assigned-task-count').innerText;    
+document.getElementById('glassdoar-btn').addEventListener('click', function() {
     
-    // const Task =parseInt(task);
-    // const assignTask = parseInt(assigntask);
-
+    alert("Board updated successfully");
 
     const Task = getInteger('navbar-task-count');
     const assignTask = getInteger('assigned-task-count');
     const taskCount = Task + 1;
     const assignTaskCount = assignTask - 1;
 
-
     document.getElementById('navbar-task-count').innerText = taskCount;
     document.getElementById('assigned-task-count').innerText = assignTaskCount; 
-    
-},
-{ once: true }
-);
 
+    this.disabled = true; 
 
+}, 
+{ once: true });
